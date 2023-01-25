f6cc16f90b77c2fb53014197448b9950c9ab3f5b
HeXudong <1514133606@qq.com>
2023-01-25 16:47:39 +0800

    feat: add exe 6.13~6.14

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,15 +1,18 @@
 import { useDispatch } from 'react-redux'
 import { add } from "../reducers/anecdoteReducer";
 import { show } from "../reducers/notificationReducer";
+import anecdoteService from '../services/anecdote'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault()
         const anecdote = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(add(anecdote))
+        // dispatch(add(anecdote))
+        const newAnecdote = await anecdoteService.createNew(anecdote)
+        dispatch(add(newAnecdote))
         dispatch(show({ message: `you added '${anecdote}'` }))
     }
 
--- redux-anecdotes/src/store.js
+++ redux-anecdotes/src/store.js
@@ -10,5 +10,4 @@ const store = configureStore({
         filter: filterReducer
     }
 })
-
 export default store
